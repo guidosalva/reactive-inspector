03f23842a02443f6e09a3498b99e90d11e5b786b
Sebastian Ruhleder <sebastian.ruhleder@googlemail.com>
2014-11-05 14:22:25 +0100

    Add comments to CustomGraph class

--- REClipse_TreeViewer/src/de/tu_darmstadt/stg/reclipse/graphview/view/CustomGraph.java
+++ REClipse_TreeViewer/src/de/tu_darmstadt/stg/reclipse/graphview/view/CustomGraph.java
@@ -1,6 +1,8 @@
 package de.tu_darmstadt.stg.reclipse.graphview.view;
 
 import java.awt.Frame;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.awt.event.MouseWheelEvent;
 import java.awt.event.MouseWheelListener;
 
@@ -24,15 +26,22 @@ public class CustomGraph extends mxGraph {
   public CustomGraph(final Composite parent) {
     super();
 
+    // create child composite
     final Composite composite = new Composite(parent, SWT.EMBEDDED | SWT.BACKGROUND);
+
+    // set layout data to fill parent composite
     composite.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
 
+    // create frame inside composite
     final Frame graphFrame = SWT_AWT.new_Frame(composite);
 
+    // initialize graph component and add it to frame
     graphComponent = new mxGraphComponent(this);
     graphFrame.add(graphComponent);
 
+    // add listeners
     addMouseWheelListener();
+    addMouseListener();
   }
 
   private void addMouseWheelListener() {
@@ -40,14 +49,27 @@ public class CustomGraph extends mxGraph {
 
       @Override
       public void mouseWheelMoved(final MouseWheelEvent e) {
+        // get rotation of mouse wheel
         final int steps = e.getWheelRotation();
 
         if (steps < 0) {
+          // zoom in if mouse wheel scrolls up
           zoomIn();
         }
         else {
+          // zoom out if mouse wheel scrolls down
           zoomOut();
         }
+      }
+    });
+  }
+
+  private void addMouseListener() {
+    graphComponent.addMouseListener(new MouseAdapter() {
+
+      @Override
+      public void mouseClicked(final MouseEvent e) {
+        // TODO Implement vertex toggle
       }
     });
   }
